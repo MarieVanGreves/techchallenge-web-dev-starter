b87b1befee726715867f1b0574b1be2aad115f5c
MarieVanGreves <121773626+MarieVanGreves@users.noreply.github.com>
2023-01-30 17:48:42 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function verification(f) {
+  if ("We will answer you shortly. Thank you.") return true;
+  else {
+    alert(
+      "The form is not complete. Please complete before submiting. Thank you."
+    );
+
+    return false;
+  }
+}
